546c58642b52a7da56c6b19d0ac4294f767e1d6a
TruongAn <truonganspkt2014@gmail.com>
2024-11-03 22:14:00 +0700

    create feature/testimonal

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,4 +3,15 @@ $(document).ready(function () {
         e.preventDefault();
         $(window).scrollTop(0);
     });
+
+    $(".testimonal__list").flickity({
+        // options
+        // cellAlign: "center",
+        contain: true,
+        wrapAround: true,
+        groupCells: 1,
+        autoPlay: 1500,
+        prevNextButtons: false,
+        pauseAutoPlayOnHover: false,
+    });
 });
